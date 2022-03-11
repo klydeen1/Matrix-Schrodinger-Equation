f43ca4ed10cbf2a7a5182841890274f3f88ef93d
Katelyn Lydeen <klydeen@hawk.iit.edu>
2022-03-11 13:51:11 -0600

    Initial Commit

---
+++ Shared/Matrix_Schrodinger_EquationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matrix_Schrodinger_EquationApp.swift
+//  Shared
+//
+//  Created by Katelyn Lydeen on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Matrix_Schrodinger_EquationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
